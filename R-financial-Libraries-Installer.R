f92aa59af80a9e005aed57df436ec1b1e8b04757
dsfintec <dsfintec@gmail.com>
2016-07-02 02:55:53 +0500

    Script to install R packages for Finance

---
+++ R-financial-Libraries-Installer.R
@@ -0,0 +1,43 @@
+#   This script will install all the required
+#   library and packages for Financial Modeling
+
+install.packages("lubridate")
+install.packages("dplR")
+install.packages("tidyR")
+install.packages("timeSeries")
+install.packages("quantmod")
+install.packages("quantmod")
+install.packages("caTools")
+install.packages("timeDate")
+install.packages("fBasics")
+install.packages("fAssets")
+install.packages("fPortfolio")
+install.packages("PerformanceAnalytics")
+install.packages("xts")
+install.packages("corrplot")
+install.packages("http://ftp.uni-bayreuth.de/math/statlib/R/CRAN/bin/windows/contrib/r-release/fEcofin_260.72.zip",
+repos=NULL, method="libcurl")
+install.packages("devtools")
+
+require(devtools)
+install_github('rCharts', 'ramnathv')
+
+install.packages("fImport")
+install.packages("fPortfolioBacktest", repos="http://r-forge.r-project.org", 
+                 type="source")
+
+
+
+
+
+library(fPortfolio)
+library(timeDate)
+library(timeSeries)
+library(fBasics)
+library(fAssets)
+
+# Download Rsymphony package from https://cran.r-project.org/bin/windows/contrib/3.2/Rsymphony_0.1-22.zip
+install.packages("C:/Users/jkhan/Downloads/Rsymphony_0.1-22.zip", repos = NULL, type = "win.binary")
+
+library(fPortfolio)
+library(corrplot)
