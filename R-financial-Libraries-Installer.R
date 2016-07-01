fecbcbabc7f72c1a49ccd6beb7180f2f57c6d3f4
dsfintec <dsfintec@gmail.com>
2016-07-02 03:15:08 +0500

    added comments

--- R-financial-Libraries-Installer.R
+++ R-financial-Libraries-Installer.R
@@ -1,5 +1,6 @@
-#   This script will install all the required
-#   library and packages for Financial Modeling
+#   This script will install the required packages for Financial Analysis,
+#   time series analysis, portfolio optimization, and back-testing.
+#   I will update it periodically.
 
 install.packages("lubridate")
 install.packages("dplR")
@@ -11,6 +12,8 @@ install.packages("caTools")
 install.packages("timeDate")
 install.packages("fBasics")
 install.packages("fAssets")
+# Download Rsymphony package from https://cran.r-project.org/bin/windows/contrib/3.2/Rsymphony_0.1-22.zip
+install.packages("C:/Users/jkhan/Downloads/Rsymphony_0.1-22.zip", repos = NULL, type = "win.binary")
 install.packages("fPortfolio")
 install.packages("PerformanceAnalytics")
 install.packages("xts")
@@ -18,26 +21,16 @@ install.packages("corrplot")
 install.packages("http://ftp.uni-bayreuth.de/math/statlib/R/CRAN/bin/windows/contrib/r-release/fEcofin_260.72.zip",
 repos=NULL, method="libcurl")
 install.packages("devtools")
-
 require(devtools)
 install_github('rCharts', 'ramnathv')
-
 install.packages("fImport")
 install.packages("fPortfolioBacktest", repos="http://r-forge.r-project.org", 
                  type="source")
-
-
-
-
 
 library(fPortfolio)
 library(timeDate)
 library(timeSeries)
 library(fBasics)
 library(fAssets)
-
-# Download Rsymphony package from https://cran.r-project.org/bin/windows/contrib/3.2/Rsymphony_0.1-22.zip
-install.packages("C:/Users/jkhan/Downloads/Rsymphony_0.1-22.zip", repos = NULL, type = "win.binary")
-
 library(fPortfolio)
 library(corrplot)
